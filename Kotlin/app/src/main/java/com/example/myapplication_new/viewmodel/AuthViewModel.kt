df38645c8d3b8c10c251e49253d19adbb4de6e83
Smiian53696 <dsw53696@student.dsw.edu.pl>
2025-05-19 15:57:20 +0200

    Create AuthViewModel.kt

---
+++ Kotlin/app/src/main/java/com/example/myapplication_new/viewmodel/AuthViewModel.kt
@@ -0,0 +1,89 @@
+package com.example.myapplication_new.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+
+class AuthViewModel : ViewModel() {
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val _authState = MutableLiveData<AuthState>()
+    val authState: LiveData<AuthState> = _authState
+
+    init {
+        checkAuthStatus()
+    }
+
+    private fun checkAuthStatus() {
+        if (auth.currentUser == null) {
+            _authState.value = AuthState.Unauthenticated
+        } else {
+            _authState.value = AuthState.Authenticated
+        }
+    }
+
+
+    fun login(email: String, password: String, emptyMsg: String, wrongMsg: String) {
+        if (email.isEmpty() || password.isEmpty()) {
+            _authState.value = AuthState.Error(emptyMsg)
+            return
+        }
+
+        _authState.value = AuthState.Loading
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    _authState.value = AuthState.Authenticated
+                } else {
+                    _authState.value = AuthState.Error((task.exception?.message ?: wrongMsg))
+                }
+
+            }
+    }
+
+
+    fun signUp(
+        email: String,
+        password: String,
+        name: String,
+        confirmPassword: String,
+        fieldsMsg: String,
+        dontMatchMsg: String,
+        wrongMsg: String
+    ) {
+        if (email.isEmpty() || password.isEmpty() || name.isEmpty() || confirmPassword.isEmpty()) {
+            _authState.value = AuthState.Error(fieldsMsg)
+            return
+        }
+        if (password != confirmPassword) {
+            _authState.value = AuthState.Error(dontMatchMsg)
+            return
+        }
+
+        _authState.value = AuthState.Loading
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    _authState.value = AuthState.Authenticated
+                } else {
+                    _authState.value =
+                        AuthState.Error((task.exception?.message ?: wrongMsg))
+                }
+
+            }
+    }
+
+    fun signOut() {
+        auth.signOut()
+        _authState.value = AuthState.Unauthenticated
+    }
+
+
+}
+
+sealed class AuthState {
+    data object Authenticated : AuthState()
+    data object Unauthenticated : AuthState()
+    data object Loading : AuthState()
+    data class Error(val message: String) : AuthState()
+}
