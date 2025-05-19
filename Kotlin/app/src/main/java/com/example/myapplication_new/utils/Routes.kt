984e8a73c92c4a5a9923d850385905670011eec0
Smiian53696 <dsw53696@student.dsw.edu.pl>
2025-05-19 15:57:18 +0200

    Create Routes.kt

---
+++ Kotlin/app/src/main/java/com/example/myapplication_new/utils/Routes.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication_new.utils
+
+object Routes {
+    const val loginPage = "login_page"
+    const val registerPage = "register_page"
+    const val toDoListPage = "to_do_list_page"
+}
